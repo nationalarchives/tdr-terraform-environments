31167bdbdd54e5043f839bdf18900a6a07523272
Sam Palmer <github@sampalmer.dev>
2019-12-06 14:23:56 +0000

    Add terraform config for keycloak

--- modules/database/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "database_availability_zones" {}
-variable "environment" {}
-variable "security_group_ids" {}
-variable "subnet_group_name" {}
-variable "common_tags" {}
---
+++ modules/keycloak/loadbalancer.tf
@@ -0,0 +1,47 @@
+resource "aws_alb" "main" {
+  name            = "tdr-keycloak-load-balancer-${var.environment}"
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.lb.id]
+  tags = merge(
+  var.common_tags,
+  map("Name", "${var.app_name}-loadbalancer")
+  )
+}
+
+
+resource "aws_alb_target_group" "keycloak_target" {
+  name        = "keycloak-target-group-${var.environment}"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200,303"
+    timeout             = "3"
+    path                = "/"
+    unhealthy_threshold = "2"
+  }
+
+  tags = merge(
+  var.common_tags,
+  map("Name", "${var.app_name}-target-group")
+  )
+}
+
+resource "aws_alb_listener" "keycloak_tls" {
+  load_balancer_arn = aws_alb.main.id
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-west-2:328920706552:certificate/74afe7f4-1506-4147-a601-60053fcef5dc"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.keycloak_target.arn
+    type             = "forward"
+  }
+
+}
