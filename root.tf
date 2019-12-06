3b4c85340e050a336e54acf96bab4763750a61a8
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-12-06 11:38:27 +0000

    Update code to make use of the s3 backend
    Terraform will use the s3 backend created by the tdr-terraform-backend project
    
    Updated README.md with initial instructions

--- root.tf
+++ root.tf
@@ -1,6 +1,8 @@
 locals {
   #Ensure that developers' workspaces always default to 'ci'
-  environment = lookup(var.workspace_to_environment_map, terraform.workspace, "ci")
+  environment         = lookup(var.workspace_to_environment_map, terraform.workspace, "ci")
+  environment_profile = lookup(var.workspace_aws_profile_map, terraform.workspace, "ci")
+
   common_tags = map(
     "Environment", local.environment,
     "Owner", "TDR",
@@ -11,35 +13,14 @@ locals {
 terraform {
   backend "s3" {
     bucket         = "tdr-terraform-state"
-    key            = "prototype-terraform.state"
+    key            = "terraform.state"
     region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "tdr-terraform-state-lock"
   }
 }
 
-data "aws_ssm_parameter" "account_number" {
-  name = "/mgmt/${local.environment}_account"
-}
-
 provider "aws" {
-  region      = "eu-west-2"
-  assume_role {
-    role_arn = "arn:aws:iam::${data.aws_ssm_parameter.account_number.value}:role/${local.environment}-terraform-role"
-  }
-  profile     = "managementprofile"
-}
-
-//Create s3 bucket to test setup is working
-resource "aws_s3_bucket" "tk_test_bucket" {
-  bucket        = "tk-test-bucket3"
-  acl           = "private"
-  force_destroy = true
-
-  tags = merge(
-    map(
-      //local.common_tags,
-      "Name", "to-be-deleted",
-    )
-  )
+  region  = "eu-west-2"
+  profile = local.environment_profile
 }
