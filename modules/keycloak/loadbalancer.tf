f0739865e85f4cb03d77cf8ff6200a0cd25dc706
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-08 12:08:11 +0000

    Reduce name length for loadbalancer
    Names can only be 32 characters long

--- modules/keycloak/loadbalancer.tf
+++ modules/keycloak/loadbalancer.tf
@@ -1,5 +1,5 @@
 resource "aws_alb" "main" {
-  name            = "tdr-keycloak-load-balancer-${var.environment}"
+  name            = "tdr-keycloak-lb-${var.environment}"
   subnets         = aws_subnet.public.*.id
   security_groups = [aws_security_group.lb.id]
   tags = merge(
