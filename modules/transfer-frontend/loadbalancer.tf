d9ddbafbd87a7bccbd5bd4c520fe580f2e97991c
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-08 12:17:48 +0000

    Reduce keycloak load balancer name

--- modules/transfer-frontend/loadbalancer.tf
+++ modules/transfer-frontend/loadbalancer.tf
@@ -1,5 +1,5 @@
 resource "aws_alb" "main" {
-  name            = "tdr-frontend-load-balancer-${var.environment}"
+  name            = "tdr-frontend-lb-${var.environment}"
   subnets         = aws_subnet.public.*.id
   security_groups = [aws_security_group.lb.id]
   tags = merge(
