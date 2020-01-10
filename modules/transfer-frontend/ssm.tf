a740badfef87685ea7bda6d5df683c46a3a9f0cc
Sam Palmer <github@sampalmer.dev>
2020-01-10 15:00:42 +0000

    Remove password and encryption in transit.
    Because redis itself doesn't support tls connections, most libraries,
    including the recommended redis library for the play cache, don't
    support it. We don't actually need it as the redis instance is inside a
    private subnet and only accessible from the front end app. Also, the
    data is encrypted at rest which should reduce the risk further.

--- modules/transfer-frontend/ssm.tf
+++ modules/transfer-frontend/ssm.tf
@@ -2,21 +2,10 @@ resource "random_password" "play_secret" {
   length = 32
 }
 
-resource "random_password" "redis_password" {
-  length = 32
-  special = false
-}
-
 resource "aws_ssm_parameter" "play_secret" {
   name  = "/${var.environment}/frontend/play_secret"
   type  = "SecureString"
   value = random_password.play_secret.result
-}
-
-resource "aws_ssm_parameter" "redis_password" {
-  name = "/${var.environment}/frontend/redis/password"
-  type = "SecureString"
-  value = random_password.redis_password.result
 }
 
 resource "aws_ssm_parameter" "redis_host" {
