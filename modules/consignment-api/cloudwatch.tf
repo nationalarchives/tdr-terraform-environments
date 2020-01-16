4012e30faceb9cbb076b1824ed6a1b2ebbcfdb40
Sam Palmer <github@sampalmer.dev>
2020-01-16 14:23:05 +0000

    Add terraform for the consignment api.

---
+++ modules/consignment-api/cloudwatch.tf
@@ -0,0 +1,10 @@
+# Set up CloudWatch group and log stream and retain logs for 30 days
+resource "aws_cloudwatch_log_group" "consignment_api_log_group" {
+  name              = "/ecs/consignment-api-${var.environment}"
+  retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_stream" "consignment_api_log_stream" {
+  name           = "tdr-consignment-api-log-stream-${var.environment}"
+  log_group_name = aws_cloudwatch_log_group.consignment_api_log_group.name
+}
