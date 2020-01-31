7c52cce0b1a66d7f6257fcf839038372ac219002
Sam Palmer <github@sampalmer.dev>
2020-01-31 12:06:17 +0000

    Ran terraform fmt
    Also changed the variable name to environment_full_name.

--- modules/keycloak/route53.tf
+++ modules/keycloak/route53.tf
@@ -3,12 +3,12 @@ data "aws_route53_zone" "keycloak_dns_zone" {
 }
 
 resource "aws_route53_record" "keycloak_dns" {
-  name = "auth"
-  type = "A"
+  name    = "auth"
+  type    = "A"
   zone_id = data.aws_route53_zone.keycloak_dns_zone.zone_id
   alias {
     evaluate_target_health = false
-    name = aws_alb.main.dns_name
-    zone_id = aws_alb.main.zone_id
+    name                   = aws_alb.main.dns_name
+    zone_id                = aws_alb.main.zone_id
   }
 }
--- modules/transfer-frontend/route53.tf
+++ modules/transfer-frontend/route53.tf
@@ -3,12 +3,12 @@ data "aws_route53_zone" "frontend_dns_zone" {
 }
 
 resource "aws_route53_record" "frontend_dns" {
-  name = ""
-  type = "A"
+  name    = ""
+  type    = "A"
   zone_id = data.aws_route53_zone.frontend_dns_zone.zone_id
   alias {
     evaluate_target_health = false
-    name = aws_alb.main.dns_name
-    zone_id = aws_alb.main.zone_id
+    name                   = aws_alb.main.dns_name
+    zone_id                = aws_alb.main.zone_id
   }
 }
