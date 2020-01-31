9b913b1a8f9bdf105f3cdf10e2ecd2082527d707
sam <github@sampalmer.dev>
2020-01-31 11:31:42 +0000

    Add route53 configuration

---
+++ modules/keycloak/route53.tf
@@ -0,0 +1,14 @@
+data "aws_route53_zone" "keycloak_dns_zone" {
+  name = "tdr-${var.environment_full}.nationalarchives.gov.uk"
+}
+
+resource "aws_route53_record" "keycloak_dns" {
+  name = "auth"
+  type = "A"
+  zone_id = data.aws_route53_zone.keycloak_dns_zone.zone_id
+  alias {
+    evaluate_target_health = false
+    name = aws_alb.main.dns_name
+    zone_id = aws_alb.main.zone_id
+  }
+}
