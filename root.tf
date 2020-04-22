21b1eb7855ddab216c530b02fc73c8784a62c830
techncl <nigel_lutale@hotmail.co.uk>
2020-04-22 11:30:31 +0100

    Replace clean bucket with dirty one

--- root.tf
+++ root.tf
@@ -81,12 +81,10 @@ module "alb_logs_s3" {
   common_tags   = local.common_tags
 }
 
-module "upload_file_s3" {
+module "upload_file_dirty_s3" {
   source        = "./tdr-terraform-modules/s3"
   project       = var.project
-  function      = "upload-files"
-  access_logs   = false
-  bucket_policy = "alb_logging_euwest2"
+  function      = "upload-files-dirty"
   common_tags   = local.common_tags
 }
 
