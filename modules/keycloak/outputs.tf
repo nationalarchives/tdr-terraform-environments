78b77e10dcda6e99efad9397f2f5846bf20d7235
TomJKing <tom.king@nationalarchives.gov.uk>
2020-05-06 10:46:51 +0100

    Rename existing client secret

--- modules/keycloak/outputs.tf
+++ modules/keycloak/outputs.tf
@@ -14,8 +14,8 @@ output "auth_url" {
   value = "https://${aws_route53_record.keycloak_dns.name}.${var.dns_zone_name_trimmed}/auth"
 }
 
-output "client_secret_path" {
-  value = aws_ssm_parameter.keycloak_client_secret.name
+output "frontend_app_client_secret_path" {
+  value = aws_ssm_parameter.keycloak_frontend_app_client_secret.name
 }
 
 output "backend_checks_client_secret_path" {
