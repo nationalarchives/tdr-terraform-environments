e76a195bb15e82180e0c8956bf08ea87de14254c
sam <github@sampalmer.dev>
2020-07-09 16:41:22 +0100

    Add dns support to the vpc
    This allows things like looking up the dns name of the efs volume by id.
    You can't specify the dns name for this so if we want the ecs file
    format build task to work, this needs to be turned on.

--- modules/shared-vpc/root.tf
+++ modules/shared-vpc/root.tf
@@ -3,7 +3,8 @@ data "aws_availability_zones" "available" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = merge(
     var.common_tags,
     map("Name", "tdr-vpc-${var.environment}")
