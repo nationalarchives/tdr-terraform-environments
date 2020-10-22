49697586faf146ffe0a9e64d7c4327f730be7a3c
TomJKing <tom.king@nationalarchives.gov.uk>
2020-10-22 10:13:13 +0100

    Clean files and remove unnecessary param in build

--- modules/consignment-api/cloudwatch.tf
+++ modules/consignment-api/cloudwatch.tf
@@ -1,6 +1,6 @@
 # Set up CloudWatch group and log stream and retain logs for 30 days
 resource "aws_cloudwatch_log_group" "consignment_api_log_group" {
-  name        = "/ecs/consignment-api-${var.environment}"
+  name              = "/ecs/consignment-api-${var.environment}"
   retention_in_days = 30
 }
 
