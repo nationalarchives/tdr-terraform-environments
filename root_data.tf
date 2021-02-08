127aefbd6974f35ca6717dee3cb29d31a145987f
sam <sam.palmer@nationalarchives.gov.uk>
2021-02-08 14:35:14 +0000

    Add NAT public IP to WAF.
    The export task needs to talk to the keycloak admin url but that is
    hidden behind the WAF which has an IP whitelist. We need to add the NAT
    public IPs which is what the ECS tasks use to get to the internet, to
    the WAF.

--- root_data.tf
+++ root_data.tf
@@ -13,3 +13,11 @@ data "aws_route53_zone" "tdr_dns_zone" {
 data "aws_ssm_parameter" "keycloak_backend_checks_client_secret" {
   name = "/${local.environment}/keycloak/backend_checks_client/secret"
 }
+
+data "aws_nat_gateway" "main_zero" {
+  tags = map("Name", "nat-gateway-0-tdr-${local.environment}")
+}
+
+data "aws_nat_gateway" "main_one" {
+  tags = map("Name", "nat-gateway-1-tdr-${local.environment}")
+}
