03f7f58cb4c1696346cc4bbd345732137e9c58c4
Sam Palmer <github@sampalmer.dev>
2021-03-03 21:01:00 +0000

    Remove data blocks from root_data and use the module outputs directly

--- root_data.tf
+++ root_data.tf
@@ -9,15 +9,3 @@ data "aws_ssm_parameter" "trusted_ips" {
 data "aws_route53_zone" "tdr_dns_zone" {
   name = local.environment == "prod" ? "${var.project}.${var.domain}" : "${var.project}-${local.environment_full_name}.${var.domain}"
 }
-
-data "aws_ssm_parameter" "keycloak_backend_checks_client_secret" {
-  name = "/${local.environment}/keycloak/backend_checks_client/secret"
-}
-
-data "aws_nat_gateway" "main_zero" {
-  tags = map("Name", "nat-gateway-0-tdr-${local.environment}")
-}
-
-data "aws_nat_gateway" "main_one" {
-  tags = map("Name", "nat-gateway-1-tdr-${local.environment}")
-}
