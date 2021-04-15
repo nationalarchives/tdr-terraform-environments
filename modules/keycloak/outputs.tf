da88aa8a157800aac7ff29d4ed4c4fec8fc6b397
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-04-15 11:54:31 +0100

    Only allow requests with auth server as host

--- modules/keycloak/outputs.tf
+++ modules/keycloak/outputs.tf
@@ -10,6 +10,10 @@ output alb_security_group_id {
   value = aws_security_group.lb.id
 }
 
+output "auth_host" {
+  value = "${aws_route53_record.keycloak_dns.name}.${var.dns_zone_name_trimmed}"
+}
+
 output "auth_url" {
   value = "https://${aws_route53_record.keycloak_dns.name}.${var.dns_zone_name_trimmed}/auth"
 }
