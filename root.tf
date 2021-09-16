7e3520a93422402884c6b4cc86b4816babb51317
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-16 11:42:06 +0100

    Add DNS and Cloudfront ACM certificate
    The second provider is needed because the certificate needs to be
    created in us-east-1.

--- root.tf
+++ root.tf
@@ -148,6 +148,17 @@ module "cloudwatch_upload" {
   s3_regional_domain_name             = module.upload_file_cloudfront_dirty_s3.s3_bucket_regional_domain_name
   environment                         = local.environment
   logging_bucket_regional_domain_name = module.upload_file_cloudfront_dirty_s3.s3_bucket_regional_domain_name
+  alias_domain_name                   = local.upload_domain
+  certificate_arn                     = module.cloudfront_certificate.certificate_arn
+}
+
+module "cloudfront_upload_dns" {
+  source                = "./tdr-terraform-modules/route53"
+  common_tags           = local.common_tags
+  environment_full_name = local.environment_full_name
+  project               = var.project
+  create_hosted_zone    = false
+  a_record_name         = "upload"
 }
 
 module "consignment_api_certificate" {
@@ -157,6 +168,18 @@ module "consignment_api_certificate" {
   dns_zone    = local.environment_domain
   domain_name = "api.${local.environment_domain}"
   common_tags = local.common_tags
+}
+
+module "cloudfront_certificate" {
+  source      = "./tdr-terraform-modules/certificatemanager"
+  common_tags = local.common_tags
+  dns_zone    = local.environment_domain
+  domain_name = local.upload_domain
+  function    = "cloudfront-upload"
+  project     = var.project
+  providers = {
+    aws = aws.useast1
+  }
 }
 
 module "consignment_api_alb" {
