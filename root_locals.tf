9aba2bd165e8d41797b880d1e246ca6c07a7ac2e
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-11-02 18:42:10 +0000

    Add new Keycloak into TDR VPC

--- root_locals.tf
+++ root_locals.tf
@@ -49,4 +49,18 @@ locals {
   trusted_ip_list = split(",", module.global_parameters.trusted_ips)
 
   ip_allowlist = concat(local.developer_ip_list, local.trusted_ip_list)
+
+  ecr_account_number = local.environment == "sbox" ? data.aws_caller_identity.current.account_id : data.aws_ssm_parameter.mgmt_account_number.value
+
+  keycloak_backend_checks_secret_name     = "/${local.environment}/keycloak/new/backend_checks_client/secret"
+  keycloak_tdr_client_secret_name         = "/${local.environment}/keycloak/new/client/secret"
+  keycloak_user_password_name             = "/${local.environment}/keycloak/new/password"
+  keycloak_admin_password_name            = "/${local.environment}/keycloak/new/admin/password"
+  keycloak_admin_user_name                = "/${local.environment}/keycloak/new/admin/user"
+  keycloak_realm_admin_client_secret_name = "/${local.environment}/keycloak/new/realm_admin_client/secret"
+  keycloak_configuration_properties_name  = "/${local.environment}/keycloak/new/configuration_properties"
+  keycloak_user_admin_client_secret_name  = "/${local.environment}/keycloak/new/user_admin_client/secret"
+  keycloak_govuk_notify_api_key_name      = "/${local.environment}/keycloak/new/govuk_notify/api_key"
+  keycloak_govuk_notify_template_id_name  = "/${local.environment}/keycloak/new/govuk_notify/template_id"
+  keycloak_reporting_client_secret_name   = "/${local.environment}/keycloak/new/reporting_client/secret"
 }
