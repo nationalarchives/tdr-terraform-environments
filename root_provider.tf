4c5ee5310d7b8dc44ceb799bf8a68a9efeb3ba14
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-16 14:32:37 +0000

    Add management role and put back assume role bits

--- root_provider.tf
+++ root_provider.tf
@@ -1,10 +1,20 @@
 provider "aws" {
   region = "eu-west-2"
+
+  assume_role {
+    role_arn     = local.assume_role
+    session_name = "terraform"
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
   alias  = "useast1"
+
+  assume_role {
+    role_arn     = local.assume_role
+    session_name = "terraform"
+  }
 }
 
 provider "github" {
