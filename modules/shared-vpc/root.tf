84850db0085d4da312c7a9b30c3a50f67271b5f1
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-08-09 15:53:43 +0100

    Add elastic IP arns.
    Advanced shield can be added to elastic IPs. The only two we have are
    these for the NAT Gateways so I've added advanced shield to them.

--- modules/shared-vpc/root.tf
+++ modules/shared-vpc/root.tf
@@ -1,6 +1,8 @@
 data "aws_availability_zones" "available" {
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
