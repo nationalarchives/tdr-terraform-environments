7744423a6953871eb4cc91902dab3296c90e6f6d
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-08-12 11:10:25 +0100

    Use multi-az in prod

--- root.tf
+++ root.tf
@@ -881,4 +881,5 @@ module "consignment_api_database" {
   kms_key_id         = module.encryption_key.kms_key_arn
   private_subnets    = module.shared_vpc.private_subnets
   security_group_ids = [module.api_database_security_group.security_group_id]
+  multi_az           = local.environment == "prod"
 }
