07ce00a2a7bf9f87f63ca72f8801f4428b242402
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-08-25 12:39:04 +0100

    Don't pass team slug for reporting.
    We need people outside the TDR team to be able to run reports on prod so
    the team restrictions need to be removed for staging and prod.
    
    The reports can only be run by users with access to the nationalarchives
    organisation so this isn't opening the report up for the whole world.

--- root_github.tf
+++ root_github.tf
@@ -428,7 +428,6 @@ module "github_reporting_environment" {
   source          = "./tdr-terraform-modules/github_environments"
   environment     = local.environment
   repository_name = "nationalarchives/tdr-reporting"
-  team_slug       = "transfer-digital-records-admins"
   secrets = {
     ACCOUNT_NUMBER = data.aws_caller_identity.current.account_id
   }
