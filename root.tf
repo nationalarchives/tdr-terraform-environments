532dc732cb6049a8f992b3192e42628668ea5379
thanhz <thanhz@hotmail.com>
2022-10-20 14:32:46 +0100

    Create new SQS queue + use tre-out arn ssm parameter

--- root.tf
+++ root.tf
@@ -407,6 +407,16 @@ module "transform_engine_retry_queue" {
   kms_key_id         = module.encryption_key.kms_key_arn
 }
 
+module "transform_engine_v2_retry_queue" {
+  source             = "./tdr-terraform-modules/sqs"
+  common_tags        = local.common_tags
+  project            = var.project
+  function           = "transform-engine-v2-retry"
+  sqs_policy         = "transform_engine_v2_retry"
+  visibility_timeout = 180 * 3
+  kms_key_id         = module.encryption_key.kms_key_arn
+}
+
 module "api_update_lambda" {
   source                                = "./tdr-terraform-modules/lambda"
   project                               = var.project
