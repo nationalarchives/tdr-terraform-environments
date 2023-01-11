fe4cb55232bf889336bc1e881ad4abf9bba18071
TomJKing <tom.king@nationalarchives.gov.uk>
2023-01-11 15:32:15 +0000

    Release to intg and staging environments

--- root_locals.tf
+++ root_locals.tf
@@ -84,6 +84,6 @@ locals {
   security_audit = local.environment == "prod" ? false : true
 
   //Feature access blocks
-  block_feature_closure_metadata     = true
-  block_feature_descriptive_metadata = true
+  block_feature_closure_metadata     = local.environment == "prod" ? true : false
+  block_feature_descriptive_metadata = local.environment == "prod" ? true : false
 }
