c8f855591cc7b55c914c4421247a535fe0bea076
TomJKing <tom.king@nationalarchives.gov.uk>
2023-03-23 09:52:33 +0000

    Remove deprecated fields

--- modules/transfer-frontend/elasticache.tf
+++ modules/transfer-frontend/elasticache.tf
@@ -1,16 +1,16 @@
 resource "aws_elasticache_replication_group" "redis_replication_group" {
-  replication_group_description = "The replication group for redis for the front end"
-  replication_group_id          = "frontend-redis-${var.environment}"
-  number_cache_clusters         = 1
-  at_rest_encryption_enabled    = true
-  engine                        = "redis"
-  node_type                     = "cache.t2.micro"
-  parameter_group_name          = "default.redis6.x"
-  engine_version                = "6.x"
-  port                          = 6379
-  security_group_ids            = aws_security_group.redis.*.id
-  subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.name
-  apply_immediately             = true
+  description                = "The replication group for redis for the front end"
+  replication_group_id       = "frontend-redis-${var.environment}"
+  num_cache_clusters         = 1
+  at_rest_encryption_enabled = true
+  engine                     = "redis"
+  node_type                  = "cache.t2.micro"
+  parameter_group_name       = "default.redis6.x"
+  engine_version             = "6.x"
+  port                       = 6379
+  security_group_ids         = aws_security_group.redis.*.id
+  subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
+  apply_immediately          = true
 }
 
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
