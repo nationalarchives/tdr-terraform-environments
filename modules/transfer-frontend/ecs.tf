2f625bf1710813780ee7b3838e611c0e4d438d0f
thanhz <tri.lam21@outlook.com>
2023-04-05 14:21:20 +0100

    collector_image variable change

--- modules/transfer-frontend/ecs.tf
+++ modules/transfer-frontend/ecs.tf
@@ -18,7 +18,7 @@ data "template_file" "app" {
   template = file("modules/transfer-frontend/templates/frontend.json.tpl")
 
   vars = {
-    collector_image                    = "${data.aws_ssm_parameter.mgmt_account_number.value}.dkr.ecr.eu-west-2.amazonaws.com/transfer-frontend-collector:${var.environment}"
+    collector_image                    = "${data.aws_ssm_parameter.mgmt_account_number.value}.dkr.ecr.eu-west-2.amazonaws.com/aws-otel-collector:${var.environment}"
     app_image                          = "${data.aws_ssm_parameter.mgmt_account_number.value}.dkr.ecr.eu-west-2.amazonaws.com/transfer-frontend:${var.environment}"
     app_port                           = local.app_port
     app_environment                    = var.environment
