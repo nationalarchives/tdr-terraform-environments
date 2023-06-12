9b086c6eeeddd03f7051ad84b5178eaf578b5bcd
TomJKing <tom.king@nationalarchives.gov.uk>
2023-06-12 08:37:42 +0100

    Remove common secrets from environment secrets

--- root_github.tf
+++ root_github.tf
@@ -89,12 +89,8 @@ module "github_keycloak_user_management_environment" {
   repository_name = "nationalarchives/tdr-keycloak-user-management"
   team_slug       = "transfer-digital-records-admins"
   secrets = {
-    TITLE_STAGE            = title(local.environment)
-    ACCOUNT_NUMBER         = data.aws_caller_identity.current.account_id
-    MANAGEMENT_ACCOUNT     = data.aws_ssm_parameter.mgmt_account_number.value
-    SLACK_FAILURE_WORKFLOW = data.aws_ssm_parameter.slack_failure_workflow.value
-    SLACK_SUCCESS_WORKFLOW = data.aws_ssm_parameter.slack_success_workflow.value
-    WORKFLOW_PAT           = data.aws_ssm_parameter.workflow_pat.value
+    TITLE_STAGE    = title(local.environment)
+    ACCOUNT_NUMBER = data.aws_caller_identity.current.account_id
   }
 }
 
