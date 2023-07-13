2437482bac81c5b821a1716f0c763c1de5c93d01
TomJKing <tom.king@nationalarchives.gov.uk>
2023-07-13 11:34:44 +0100

    Rename key

--- root_encryption.tf
+++ root_encryption.tf
@@ -1,6 +1,6 @@
 module "s3_external_kms_key" {
   source   = "./da-terraform-modules/kms"
-  key_name = "tdr-s3-data-external-${local.environment}"
+  key_name = "tdr-s3-external-kms-${local.environment}"
   tags     = local.common_tags
   default_policy_variables = {
     user_roles    = [module.notification_lambda.notifications_lambda_role_arn[0], module.consignment_export_task_role.role.arn]
