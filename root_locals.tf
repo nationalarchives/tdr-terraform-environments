85837a5490d62bf2d994f50e3d168289c5b7789b
TomJKing <tom.king@nationalarchives.gov.uk>
2023-10-03 13:16:51 +0100

    Retrieve TRE key arn instead of alias

--- root_locals.tf
+++ root_locals.tf
@@ -113,7 +113,7 @@ locals {
 
   // event bus hosted on tre environments
   da_event_bus_arn     = module.tre_configuration.terraform_config[local.tre_environment]["da_eventbus"]
-  da_event_bus_kms_key = module.tre_configuration.terraform_config[local.tre_environment]["da_eventbus_kms_key_alias_arn"]
+  da_event_bus_kms_key = module.tre_configuration.terraform_config["${local.tre_environment}_da_eventbus_kms_arn"]
 
   da_reference_generator_url = module.tdr_configuration.terraform_config["reference_generator_${local.environment}_url"]
 
