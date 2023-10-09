868c8541e492a3f6b892633ed40e6131301ab256
TomJKing <tom.king@nationalarchives.gov.uk>
2023-10-09 15:10:42 +0100

    Fix ssm parameter names

--- root_export_bucket_access.tf
+++ root_export_bucket_access.tf
@@ -3,19 +3,19 @@ module "aws_sso_export_roles_ssm_parameters" {
   source = "./da-terraform-modules/ssm_parameter"
   parameters = [
     {
-      name        = "${local.environment}/admin_role",
+      name        = "/${local.environment}/admin_role",
       description = "AWS SSO admin role. Value to be added manually"
       type        = "SecureString"
       value       = "placeholder"
     },
     {
-      name        = "${local.environment}/developer_role",
+      name        = "/${local.environment}/developer_role",
       description = "AWS SSO developer role. Value to be added manually"
       type        = "SecureString"
       value       = "placeholder"
     },
     {
-      name        = "${local.environment}/export_role",
+      name        = "/${local.environment}/export_role",
       description = "AWS SSO export role. Value to be added manually"
       type        = "SecureString"
       value       = "placeholder"
