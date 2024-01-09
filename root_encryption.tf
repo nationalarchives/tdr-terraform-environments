70ba418c44c86ce9e7f583bf5480682d41b991a0
TomJKing <tom.king@nationalarchives.gov.uk>
2024-01-09 13:45:41 +0000

    KMS key to encrypt internal TDR s3 buckets

--- root_encryption.tf
+++ root_encryption.tf
@@ -11,3 +11,14 @@ module "s3_external_kms_key" {
     service_names = ["cloudwatch"]
   }
 }
+
+module "s3_internal_kms_key" {
+  source   = "./da-terraform-modules/kms"
+  key_name = "tdr-s3-internal-kms-${local.environment}"
+  tags     = local.common_tags
+  default_policy_variables = {
+    user_roles    = concat(local.aws_sso_internal_bucket_access_roles)
+    ci_roles      = [local.assume_role]
+    service_names = ["cloudwatch"]
+  }
+}
