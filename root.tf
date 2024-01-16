b69b5b45ed4312e58f2ef8387bb887817dbaca11
Tom Hallett <tehallett+github@gmail.com>
2024-01-16 12:56:13 +0000

    TDR-3671 Correct formatting

--- root.tf
+++ root.tf
@@ -425,10 +425,10 @@ module "reporting_lambda" {
   reporting_client_secret_path     = local.keycloak_reporting_client_secret_name
   slack_bot_token                  = module.keycloak_ssm_parameters.params[local.slack_bot_token_name].value
   tdr_reporting_slack_channel_id   = module.reporting_lambda_ssm_parameters.params[local.tdr_reporting_slack_channel_id].value
-  timeout_seconds                = 120
-  kms_key_arn                    = module.encryption_key.kms_key_arn
-  private_subnet_ids             = module.shared_vpc.private_backend_checks_subnets
-  vpc_id                         = module.shared_vpc.vpc_id
+  timeout_seconds                  = 120
+  kms_key_arn                      = module.encryption_key.kms_key_arn
+  private_subnet_ids               = module.shared_vpc.private_backend_checks_subnets
+  vpc_id                           = module.shared_vpc.vpc_id
 }
 
 
