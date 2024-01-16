722921b9255fa69d74423f149165e04eac6eb0cb
Tom Hallett <tehallett+github@gmail.com>
2024-01-16 12:53:33 +0000

    TDR-3671 Add new Slack Channel ID SSM Param

---
+++ root_ssm_parameters.tf
@@ -0,0 +1,24 @@
+locals {
+
+  manual_input_tag = tomap(
+    {
+      "ManualInput" = true
+    }
+  )
+
+  tdr_reporting_slack_channel_id = "/${local.environment}/slack/tdr_reporting/channelid"
+
+}
+
+module "reporting_lambda_ssm_parameters" {
+  source = "./da-terraform-modules/ssm_parameter"
+  parameters = [
+    {
+      name        = local.tdr_reporting_slack_channel_id,
+      description = "Slack channel id where reports will be sent. Value to be added manually"
+      type        = "SecureString"
+      value       = "CXXXXXXXXXX"
+    }
+  ]
+  tags = merge(local.common_tags, local.manual_input_tag)
+}
