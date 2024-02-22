ab22620d2d96c6489a15de45300cb9a60c235e93
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2024-02-22 15:46:44 +0000

    Terraform fmt

--- root_encryption.tf
+++ root_encryption.tf
@@ -27,8 +27,8 @@ module "s3_internal_kms_key" {
       module.consignment_export_task_role.role.arn,
       module.draft_metadata_validator_lambda.lambda_role_arn
     ], local.aws_sso_internal_bucket_access_roles, local.e2e_testing_role_arns)
-    ci_roles                 = [local.assume_role]
-    service_names            = ["cloudwatch"]
+    ci_roles      = [local.assume_role]
+    service_names = ["cloudwatch"]
   }
 }
 
@@ -43,8 +43,8 @@ module "s3_upload_kms_key" {
       module.file_format_v2.lambda_role_arn,
       module.checksum_v2.lambda_role_arn
     ], local.aws_sso_internal_bucket_access_roles)
-    ci_roles      = [local.assume_role]
-    service_names = ["cloudwatch"]
+    ci_roles                 = [local.assume_role]
+    service_names            = ["cloudwatch"]
     cloudfront_distributions = [module.cloudfront_upload.cloudfront_arn]
   }
 }
