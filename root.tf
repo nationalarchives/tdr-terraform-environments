3f009692348c7c63164f36b71140ce827a656bca
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2024-05-20 14:38:02 +0100

    [TDR-222] Support sending of transfer complete gov uk notify email from notifications lambda

--- root.tf
+++ root.tf
@@ -530,12 +530,10 @@ module "notification_lambda" {
   standard_export_s3_bucket_name = module.export_bucket.s3_bucket_name
   da_event_bus_arn               = local.da_event_bus_arn
   da_event_bus_kms_key_arn       = local.da_event_bus_kms_key
-  vpc_config = [
-    {
-      subnet_ids         = [module.shared_vpc.vpc_id]
-      security_group_ids = [module.outbound_only_security_group.security_group_id]
-    }
-  ]
+  notifications_vpc_config = {
+    subnet_ids         = module.shared_vpc.private_subnets
+    security_group_ids = [module.outbound_only_security_group.security_group_id]
+  }
 }
 
 module "tdr_public_nacl" {
