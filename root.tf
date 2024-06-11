d19c57175b3db8b0c92b38bdae4628e022cc158f
TomJKing <51711388+TomJKing@users.noreply.github.com>
2024-06-11 15:50:42 +0100

    Tdrd 222 add sns publish policy (#466)
    * [TDRD 222] Add policy to enable frontend publish to notifications SNS topic
    
    * terraform fmt
    
    * Add .arn
    
    * Add kms key use policy
    
    * Terraform fmt
    
    * Capitalise KMS
    
    ---------
    
    Co-authored-by: Annie Hawes <annie.hawes@nationalarchives.gov.uk>

--- root.tf
+++ root.tf
@@ -96,6 +96,7 @@ module "frontend" {
   draft_metadata_s3_kms_keys        = jsonencode([module.s3_internal_kms_key.kms_key_arn])
   draft_metadata_s3_bucket_name     = local.draft_metadata_s3_bucket_name
   notification_sns_topic_arn        = module.notifications_topic.sns_arn
+  notifications_topic_kms_key_arn   = module.encryption_key.kms_key_arn
 }
 
 module "alb_logs_s3" {
