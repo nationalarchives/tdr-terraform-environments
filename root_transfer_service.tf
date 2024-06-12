5d8630fecc9155db72b8ce643329c50655527489
TomJKing <tom.king@nationalarchives.gov.uk>
2024-06-12 09:09:21 +0100

    Add tagging to IAM policies

--- root_transfer_service.tf
+++ root_transfer_service.tf
@@ -30,6 +30,7 @@ module "transfer_service_execution_policy" {
   count         = local.transfer_service_count
   source        = "./da-terraform-modules/iam_policy"
   name          = "TDRTransferServiceECSExecutionPolicy${title(local.environment)}"
+  tags          = local.common_tags
   policy_string = templatefile("./templates/iam_policy/transfer_service_ecs_execution_policy.json.tpl", { log_group_arn = "${module.transfer_service_cloudwatch[0].log_group_arn}:*", management_account_number = data.aws_ssm_parameter.mgmt_account_number.value })
 }
 
@@ -37,6 +38,7 @@ module "transfer_service_task_policy" {
   count  = local.transfer_service_count
   source = "./da-terraform-modules/iam_policy"
   name   = "TDRTransferServiceECSTaskPolicy${title(local.environment)}"
+  tags   = local.common_tags
   policy_string = templatefile(
   "${path.module}/templates/iam_policy/transfer_service_ecs_task_policy.json.tpl", { account_id = data.aws_caller_identity.current.account_id, environment = local.environment })
 }
