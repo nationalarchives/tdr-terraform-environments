4476d7e86ca202b872c918c3364abe95608f8ac8
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2024-07-03 13:12:00 +0100

    TDRD-227 - Add "blockMetadataReview" feature access block for frontend

--- root_locals.tf
+++ root_locals.tf
@@ -133,6 +133,7 @@ locals {
   //feature access blocks
   block_shared_keycloak_pages       = local.environment == "intg" ? false : true
   block_draft_metadata_upload       = local.environment == "prod" ? true : false
+  block_metadata_review             = local.environment == "prod" ? true : false
   block_automate_judgment_transfers = local.environment == false
   draft_metadata_s3_bucket_name     = "${var.project}-draft-metadata-${local.environment}"
 
