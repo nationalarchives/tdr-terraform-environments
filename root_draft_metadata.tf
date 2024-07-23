0958274e6ef1bbd4a12cb5c227c006967eb04ec0
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-07-23 14:39:14 +0100

    rename draft-metadata-validator for deletion

--- root_draft_metadata.tf
+++ root_draft_metadata.tf
@@ -1,6 +1,6 @@
 module "draft_metadata_validator_lambda" {
   source          = "./da-terraform-modules/lambda"
-  function_name   = "tdr-draft-metadata-validator-${local.environment}"
+  function_name   = "tdr-draft-metadata-validator-old-${local.environment}"
   handler         = "uk.gov.nationalarchives.draftmetadatavalidator.Lambda::handleRequest"
   runtime         = local.runtime_java_11
   tags            = local.common_tags
