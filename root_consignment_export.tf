a4a0372bc260220e991838fed2a8de9bcff6228a
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-09-03 16:02:40 +0100

    reverted-consignment-export-execution-policy

--- root_consignment_export.tf
+++ root_consignment_export.tf
@@ -35,15 +35,9 @@ module "consignment_export_task_role" {
 }
 
 module "consignment_export_execution_policy" {
-  source = "./tdr-terraform-modules/iam_policy"
-  name   = "TDRConsignmentExportECSExecutionPolicy${title(local.environment)}"
-  policy_string = templatefile("./templates/iam_policy/consignment_export_execution_policy.json.tpl", {
-    log_group_arn             = "${module.consignment_export_cloudwatch.log_group_arn}:*",
-    file_system_arn           = module.export_efs.file_system_arn,
-    management_account_number = data.aws_ssm_parameter.mgmt_account_number.value,
-    aws_guardduty_ecr_arn     = local.aws_guardduty_ecr_arn
-    }
-  )
+  source        = "./tdr-terraform-modules/iam_policy"
+  name          = "TDRConsignmentExportECSExecutionPolicy${title(local.environment)}"
+  policy_string = templatefile("./tdr-terraform-modules/iam_policy/templates/consignment_export_execution_policy.json.tpl", { log_group_arn = "${module.consignment_export_cloudwatch.log_group_arn}:*", file_system_arn = module.export_efs.file_system_arn, management_account_number = data.aws_ssm_parameter.mgmt_account_number.value })
 }
 
 module "consignment_export_task_policy" {
