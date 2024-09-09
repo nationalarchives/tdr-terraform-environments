4e49d8e2da1f766c4ca18655bb6c1cc39a08cda4
thanhz <thanhz@users.noreply.github.com>
2024-09-09 11:28:43 +0100

    Update `update_waf_and_security_groups_policy` (#509)
    Required for adding IP address to WAF IPSet in the e2e tests

--- root_github.tf
+++ root_github.tf
@@ -2,9 +2,11 @@ module "github_update_waf_and_security_groups_policy" {
   source = "./tdr-terraform-modules/iam_policy"
   name   = "TDRUpdateWAFAndSecurityGroupsPolicy${title(local.environment)}"
   policy_string = templatefile("${path.module}/templates/iam_policy/update_waf_and_security_groups_policy.json.tpl", {
-    rule_group_arn    = module.waf.rule_group_arn, ip_set_arn = module.waf.ip_set_arn,
+    rule_group_arn    = module.waf.rule_group_arn,
+    ip_set_arn        = module.waf.ip_set_arn,
     account_id        = data.aws_caller_identity.current.account_id,
     security_group_id = module.frontend.alb_security_group_id
+    environment       = local.environment
   })
 }
 
