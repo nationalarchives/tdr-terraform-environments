3cb88969ecebc736316fdf41d5de354cc85bc4c8
TomJKing <tom.king@nationalarchives.gov.uk>
2024-10-14 11:06:53 +0100

    Change tag name

--- root_s3_life_cycle.tf
+++ root_s3_life_cycle.tf
@@ -1,23 +1,67 @@
 locals {
-  // Apply to intg /staging environments only initially
-  life_cycle_count        = local.environment == "prod" ? 0 : 1
-  dirty_bucket_expiration = local.environment == "prod" ? 7 : 1
+  clean_buckets              = [module.upload_bucket]
+  clean_bucket_expiration    = local.environment == "prod" ? 30 : 7
+  clean_bucket_policy_status = "Disabled"
+
+  dirty_buckets              = [module.upload_file_cloudfront_dirty_s3]
+  dirty_bucket_expiration    = local.environment == "prod" ? 7 : 1
+  dirty_bucket_policy_status = local.environment == "intg" ? "Enabled" : "Disabled"
+
+  export_buckets              = [module.export_bucket, module.flat_format_export_bucket]
+  export_bucket_expiration    = local.environment == "prod" ? 30 : 7
+  export_bucket_policy_status = "Disabled"
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "upload_file_cloudfront_dirty_s3" {
-  count  = local.life_cycle_count
-  bucket = module.upload_file_cloudfront_dirty_s3.s3_bucket_id
+resource "aws_s3_bucket_lifecycle_configuration" "dirty_s3_buckets" {
+  for_each = { for bucket in local.dirty_buckets : bucket.s3_bucket_name => bucket }
+  bucket   = each.value.s3_bucket_id
   rule {
-    id     = "delete-backend-checks-completed"
-    status = "Enabled"
+    id     = "delete-dirty-buckets"
+    status = local.dirty_bucket_policy_status
     filter {
       tag {
-        key   = "BackendChecks"
+        key   = "Copy"
         value = "Completed"
       }
     }
     expiration {
       days = local.dirty_bucket_expiration
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "clean_s3_buckets" {
+  for_each = { for bucket in local.clean_buckets : bucket.s3_bucket_name => bucket }
+  bucket   = each.value.s3_bucket_id
+  rule {
+    id     = "delete-clean-buckets"
+    status = local.clean_bucket_policy_status
+    filter {
+      tag {
+        key   = "Preservation"
+        value = "Completed"
+      }
+    }
+    expiration {
+      days = local.clean_bucket_expiration
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "export_s3_buckets" {
+  for_each = { for bucket in local.export_buckets : bucket.s3_bucket_name => bucket }
+  bucket   = each.value.s3_bucket_id
+  rule {
+    id     = "delete-export-buckets"
+    status = local.export_bucket_policy_status
+    filter {
+      tag {
+        key   = "Preservation"
+        value = "Completed"
+      }
+    }
+    expiration {
+      days = local.export_bucket_expiration
     }
   }
 }
