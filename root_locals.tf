12da4db32ae41e0632d844855317b196c2d0ea46
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2024-10-21 16:36:31 +0100

    [TDRD 381] Turn on metadata review for all environments (#527)

--- root_locals.tf
+++ root_locals.tf
@@ -139,8 +139,8 @@ locals {
   //feature access blocks
   block_shared_keycloak_pages   = local.environment == "intg" ? false : true
   block_draft_metadata_upload   = local.environment == "prod" ? true : false
-  block_metadata_review         = local.environment == "prod" ? true : false
-  block_skip_metadata_review    = local.environment == "prod" ? true : false
+  block_metadata_review         = false
+  block_skip_metadata_review    = false
   draft_metadata_s3_bucket_name = "${var.project}-draft-metadata-${local.environment}"
 
   flat_format_bucket_name          = "tdr-export-${local.environment}"
