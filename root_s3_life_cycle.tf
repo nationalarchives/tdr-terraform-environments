258c3e8f4be812d3c379f8b5fcd9c415fff4868b
TomJKing <tom.king@nationalarchives.gov.uk>
2024-11-22 12:48:16 +0000

    Add rule for quarantine buckets

--- root_s3_life_cycle.tf
+++ root_s3_life_cycle.tf
@@ -1,17 +1,22 @@
 locals {
-  delete_object_tag_key      = "Delete"
-  delete_object_tag_value    = "True"
-  clean_buckets              = [module.upload_bucket]
-  clean_bucket_expiration    = local.environment == "prod" ? 30 : 7
-  clean_bucket_policy_status = "Disabled"
+  delete_object_tag_key   = "Delete"
+  delete_object_tag_value = "True"
 
-  dirty_buckets              = [module.upload_file_cloudfront_dirty_s3]
-  dirty_bucket_expiration    = local.environment == "prod" ? 7 : 1
-  dirty_bucket_policy_status = local.environment == "intg" ? "Enabled" : "Disabled"
+  clean_buckets                = [module.upload_bucket]
+  clean_bucket_expiration_days = local.environment == "prod" ? 30 : 7
+  clean_bucket_policy_status   = "Disabled"
 
-  export_buckets              = [module.export_bucket, module.flat_format_export_bucket]
-  export_bucket_expiration    = local.environment == "prod" ? 30 : 7
-  export_bucket_policy_status = "Disabled"
+  dirty_buckets                = [module.upload_file_cloudfront_dirty_s3]
+  dirty_bucket_expiration_days = local.environment == "prod" ? 7 : 1
+  dirty_bucket_policy_status   = local.environment == "intg" ? "Enabled" : "Disabled"
+
+  export_buckets                = [module.export_bucket, module.flat_format_export_bucket]
+  export_bucket_expiration_days = local.environment == "prod" ? 30 : 7
+  export_bucket_policy_status   = "Disabled"
+
+  quarantine_buckets                = [module.upload_bucket_quarantine]
+  quarantine_bucket_expiration_days = local.environment == "prod" ? 30 : 7
+  quarantine_buckets_policy_status  = "Disabled"
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "dirty_s3_buckets" {
@@ -27,7 +32,25 @@ resource "aws_s3_bucket_lifecycle_configuration" "dirty_s3_buckets" {
       }
     }
     expiration {
-      days = local.dirty_bucket_expiration
+      days = local.dirty_bucket_expiration_days
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "quarantine_s3_buckets" {
+  for_each = { for bucket in local.quarantine_buckets : bucket.s3_bucket_name => bucket }
+  bucket   = each.value.s3_bucket_id
+  rule {
+    id     = "delete-quarantine-buckets-objects"
+    status = local.quarantine_buckets_policy_status
+    filter {
+      tag {
+        key   = local.delete_object_tag_key
+        value = local.delete_object_tag_value
+      }
+    }
+    expiration {
+      days = local.quarantine_bucket_expiration_days
     }
   }
 }
@@ -45,7 +68,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "clean_s3_buckets" {
       }
     }
     expiration {
-      days = local.clean_bucket_expiration
+      days = local.clean_bucket_expiration_days
     }
   }
 }
@@ -63,7 +86,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "export_s3_buckets" {
       }
     }
     expiration {
-      days = local.export_bucket_expiration
+      days = local.export_bucket_expiration_days
     }
   }
 }
