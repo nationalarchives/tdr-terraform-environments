5d5503ad4b95fedc00d4356e501a60e528d0e7e2
TomJKing <tom.king@nationalarchives.gov.uk>
2024-11-22 11:50:57 +0000

    Update tagging filters

--- root_s3_life_cycle.tf
+++ root_s3_life_cycle.tf
@@ -1,4 +1,6 @@
 locals {
+  delete_object_tag_key      = "Delete"
+  delete_object_tag_value    = "True"
   clean_buckets              = [module.upload_bucket]
   clean_bucket_expiration    = local.environment == "prod" ? 30 : 7
   clean_bucket_policy_status = "Disabled"
@@ -16,12 +18,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "dirty_s3_buckets" {
   for_each = { for bucket in local.dirty_buckets : bucket.s3_bucket_name => bucket }
   bucket   = each.value.s3_bucket_id
   rule {
-    id     = "delete-dirty-buckets"
+    id     = "delete-dirty-buckets-objects"
     status = local.dirty_bucket_policy_status
     filter {
       tag {
-        key   = "Copy"
-        value = "Completed"
+        key   = local.delete_object_tag_key
+        value = local.delete_object_tag_value
       }
     }
     expiration {
@@ -34,12 +36,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "clean_s3_buckets" {
   for_each = { for bucket in local.clean_buckets : bucket.s3_bucket_name => bucket }
   bucket   = each.value.s3_bucket_id
   rule {
-    id     = "delete-clean-buckets"
+    id     = "delete-clean-buckets-objects"
     status = local.clean_bucket_policy_status
     filter {
       tag {
-        key   = "Preservation"
-        value = "Completed"
+        key   = local.delete_object_tag_key
+        value = local.delete_object_tag_value
       }
     }
     expiration {
@@ -52,12 +54,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "export_s3_buckets" {
   for_each = { for bucket in local.export_buckets : bucket.s3_bucket_name => bucket }
   bucket   = each.value.s3_bucket_id
   rule {
-    id     = "delete-export-buckets"
+    id     = "delete-export-buckets-objects"
     status = local.export_bucket_policy_status
     filter {
       tag {
-        key   = "Preservation"
-        value = "Completed"
+        key   = local.delete_object_tag_key
+        value = local.delete_object_tag_value
       }
     }
     expiration {
