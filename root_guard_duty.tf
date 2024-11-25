d057ff602cf8453c33685e15f0280b59fc7b2428
TomJKing <tom.king@nationalarchives.gov.uk>
2024-11-25 12:20:17 +0000

    Latest version of AWS provider allows full creation
    Can create the AWS GuardDuty s3 malware scan resource with Terraform

--- root_guard_duty.tf
+++ root_guard_duty.tf
@@ -1,14 +1,27 @@
-# Note: currently cannot define the s3 malware scan rules as using an out of date AWS provider version.
-# To implement the s3 malware scan rules do the following steps:
-# 1. Run this Terraform
-# 2. In the AWS console add the Guard Duty malware scan protection to the TDR dirty buckets
-# 3. In the options to set up the malware scan, for the role, use the created role in this Terraform: TDRGuardDutyS3MalwareScanRole{environment}
-
 locals {
-  malware_scan_bucket_enabled_names = [
-    module.upload_file_cloudfront_dirty_s3.s3_bucket_name,
-    module.draft_metadata_bucket.s3_bucket_name
+  malware_scan_bucket_enabled = [
+    module.upload_file_cloudfront_dirty_s3,
+    module.draft_metadata_bucket
   ]
+}
+
+resource "aws_guardduty_malware_protection_plan" "guard_duty_s3_malware_scan" {
+  for_each = { for bucket in local.malware_scan_bucket_enabled : bucket.s3_bucket_arn => bucket }
+  role     = module.aws_guard_duty_s3_malware_scan_role.role_arn
+
+  protected_resource {
+    s3_bucket {
+      bucket_name = each.value.s3_bucket_id
+    }
+  }
+
+  actions {
+    tagging {
+      status = "ENABLED"
+    }
+  }
+
+  tags = local.common_tags
 }
 
 module "aws_guard_duty_s3_malware_scan_role" {
@@ -30,10 +43,10 @@ module "aws_guard_duty_s3_malware_scan_policy" {
   policy_string = templatefile("./templates/iam_policy/guard_duty_s3_malware_scan_policy.json.tpl", {
     account_id                 = data.aws_caller_identity.current.account_id,
     bucket_encryption_key_arns = [module.s3_upload_kms_key.kms_key_arn, module.s3_internal_kms_key.kms_key_arn]
-    enabled_bucket_arns        = [for bucket_name in local.malware_scan_bucket_enabled_names : "arn:aws:s3:::${bucket_name}"]
-    enabled_bucket_object_arns = [for bucket_name in local.malware_scan_bucket_enabled_names : "arn:aws:s3:::${bucket_name}/*"]
-    malware_validation_objects = [for bucket_name in local.malware_scan_bucket_enabled_names :
-      "arn:aws:s3:::${bucket_name}/malware-protection-resource-validation-object"
+    enabled_bucket_arns        = [for bucket in local.malware_scan_bucket_enabled : bucket.s3_bucket_arn]
+    enabled_bucket_object_arns = [for bucket in local.malware_scan_bucket_enabled : "${bucket.s3_bucket_arn}/*"]
+    malware_validation_objects = [for bucket in local.malware_scan_bucket_enabled :
+      "arn:aws:s3:::${bucket.s3_bucket_name}/malware-protection-resource-validation-object"
     ]
   })
 }
@@ -41,7 +54,7 @@ module "aws_guard_duty_s3_malware_scan_policy" {
 module "aws_guard_duty_s3_malware_scan_threat_found_event" {
   source = "./da-terraform-modules/cloudwatch_events"
   event_pattern = templatefile("${path.module}/templates/guard_duty/guard_duty_s3_malware_scan_pattern.json.tpl", {
-    bucket_names = local.malware_scan_bucket_enabled_names
+    bucket_names = [for bucket in local.malware_scan_bucket_enabled : bucket.s3_bucket_name]
   })
   sns_topic_event_target_arn = module.notifications_topic.sns_arn
   rule_name                  = "guard-duty-s3-malware-threat-found"
