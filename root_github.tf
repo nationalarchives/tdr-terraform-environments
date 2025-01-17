5367e3acdb91d3d3127c346befe26c318a1833d9
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-01-17 12:36:54 +0000

    no need for rule group

--- root_github.tf
+++ root_github.tf
@@ -5,7 +5,6 @@ module "github_update_waf_and_security_groups_policy" {
     ip_set_arn             = module.waf.ip_set_arn,
     rule_group_arn         = module.waf.rule_group_arn,
     blocked_ip_set_arn     = module.waf.blocked_ip_set_arn,
-    blocked_rule_group_arn = module.waf.blocked_rule_group_arn,
     account_id             = data.aws_caller_identity.current.account_id,
     security_group_id      = module.frontend.alb_security_group_id,
     environment            = local.environment
