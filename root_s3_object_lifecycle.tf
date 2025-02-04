78be9ccef8a08ce7b3fee043c648095099e10ba9
TomJKing <tom.king@nationalarchives.gov.uk>
2025-02-04 08:20:16 +0000

    Use dynamic lifecycle rules
    s3 modules sets rule by default, this causes conflict if create rule resource separately from the s3 module
    
    Dynamically implement rules to avoid this

--- root_s3_object_lifecycle.tf
+++ root_s3_object_lifecycle.tf
@@ -4,6 +4,18 @@ locals {
   backend_checks_bucket_policy_status = "Enabled"
 
   dirty_bucket_policy_status = local.environment == "intg" ? "Enabled" : "Disabled"
+
+  dirty_bucket_lifecycle_rules = [
+    {
+      id     = "delete-dirty-buckets-objects"
+      status = local.dirty_bucket_policy_status
+      expiration = {
+        days = local.default_expiration_days
+      }
+      noncurrent_version_expiration = {
+        noncurrent_days = local.default_expiration_days
+      }
+  }]
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "backend_checks_results_s3_bucket" {
@@ -11,20 +23,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "backend_checks_results_s3_buck
   rule {
     id     = "delete-backend-checks-results-bucket-objects"
     status = local.backend_checks_bucket_policy_status
-    expiration {
-      days = local.default_expiration_days
-    }
-    noncurrent_version_expiration {
-      noncurrent_days = local.default_expiration_days
-    }
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "dirty_s3_buckets" {
-  bucket = module.upload_file_cloudfront_dirty_s3.s3_bucket_id
-  rule {
-    id     = "delete-dirty-buckets-objects"
-    status = local.dirty_bucket_policy_status
     expiration {
       days = local.default_expiration_days
     }
