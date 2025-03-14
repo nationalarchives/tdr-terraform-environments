9bfebe2ac3b757a49c7ae4ea92efc7b4424e0610
TomJKing <tom.king@nationalarchives.gov.uk>
2025-03-14 14:06:38 +0000

    Life cycle rules for data buckets
    There have been multiple instances of non-test data being uploaded to TDR's lower environments
    
    Removing this data is time consuming and prone to error
    
    Additionally impossible to police what is uploaded to TDR
    
    Add blanket object life cycle rules to delete data from s3 automatically
    
    This will reduce the risks of TDR's lower environments holding data that should not have been uploaded

--- root_s3_object_lifecycle.tf
+++ root_s3_object_lifecycle.tf
@@ -2,8 +2,33 @@ locals {
   default_expiration_days = local.environment == "prod" ? 30 : 7
 
   backend_checks_bucket_policy_status = "Enabled"
+  clean_bucket_policy_status          = local.environment == "prod" ? "Disabled" : "Enabled"
+  dirty_bucket_policy_status          = local.environment == "prod" ? "Disabled" : "Enabled"
+  export_bucket_policy_status         = local.environment == "prod" ? "Disabled" : "Enabled"
+  quarantine_bucket_policy_status     = local.environment == "prod" ? "Disabled" : "Enabled"
 
-  dirty_bucket_policy_status = local.environment == "prod" ? "Disabled" : "Enabled"
+  backend_checks_results_bucket_lifecycle_rules = [{
+    id     = "delete-backend-checks-results-bucket-objects"
+    status = local.backend_checks_bucket_policy_status
+    expiration = {
+      days = local.default_expiration_days
+    }
+    noncurrent_version_expiration = {
+      noncurrent_days = local.default_expiration_days
+    }
+  }]
+
+  clean_bucket_lifecycle_rules = [
+    {
+      id     = "clean-dirty-buckets-objects"
+      status = local.clean_bucket_policy_status
+      expiration = {
+        days = local.default_expiration_days
+      }
+      noncurrent_version_expiration = {
+        noncurrent_days = local.default_expiration_days
+      }
+  }]
 
   dirty_bucket_lifecycle_rules = [
     {
@@ -17,14 +42,27 @@ locals {
       }
   }]
 
-  backend_checks_results_bucket_lifecycle_rules = [{
-    id     = "delete-backend-checks-results-bucket-objects"
-    status = local.backend_checks_bucket_policy_status
-    expiration = {
-      days = local.default_expiration_days
-    }
-    noncurrent_version_expiration = {
-      noncurrent_days = local.default_expiration_days
-    }
+  export_bucket_lifecycle_rules = [
+    {
+      id     = "delete-export-buckets-objects"
+      status = local.export_bucket_policy_status
+      expiration = {
+        days = local.default_expiration_days
+      }
+      noncurrent_version_expiration = {
+        noncurrent_days = local.default_expiration_days
+      }
+  }]
+
+  quarantine_bucket_lifecycle_rules = [
+    {
+      id     = "quarantine-export-buckets-objects"
+      status = local.quarantine_bucket_policy_status
+      expiration = {
+        days = local.default_expiration_days
+      }
+      noncurrent_version_expiration = {
+        noncurrent_days = local.default_expiration_days
+      }
   }]
 }
