ff2e8e42d2f77fc3148c0a30e4b96c4953ddb81a
TomJKing <tom.king@nationalarchives.gov.uk>
2025-03-19 09:46:48 +0000

    Have separate rules for 'prod' and 'non-prod'
    Prodution life cycle rules will be different from the non-prod rules

--- root.tf
+++ root.tf
@@ -125,7 +125,7 @@ module "upload_bucket" {
   bucket_key_enabled = local.internal_bucket_key_enabled
   kms_key_id         = local.internal_s3_encryption_key_arn
   common_tags        = local.common_tags
-  lifecycle_rules    = local.clean_bucket_lifecycle_rules
+  lifecycle_rules    = local.environment == "prod" ? [] : local.non_prod_default_bucket_lifecycle_rules
 }
 
 module "upload_bucket_quarantine" {
@@ -135,7 +135,7 @@ module "upload_bucket_quarantine" {
   bucket_key_enabled = local.internal_bucket_key_enabled
   kms_key_id         = local.internal_s3_encryption_key_arn
   common_tags        = local.common_tags
-  lifecycle_rules    = local.quarantine_bucket_lifecycle_rules
+  lifecycle_rules    = local.environment == "prod" ? [] : local.non_prod_default_bucket_lifecycle_rules
 }
 
 module "upload_file_cloudfront_dirty_s3" {
@@ -517,7 +517,7 @@ module "flat_format_export_bucket" {
     bucket_name       = local.flat_format_bucket_name
     read_access_roles = [local.dr2_copy_files_role]
   })
-  lifecycle_rules = local.export_bucket_lifecycle_rules
+  lifecycle_rules = local.environment == "prod" ? [] : local.non_prod_default_bucket_lifecycle_rules
 }
 
 module "flat_format_export_bucket_judgment" {
@@ -525,7 +525,7 @@ module "flat_format_export_bucket_judgment" {
   bucket_name     = local.flat_format_judgment_bucket_name
   kms_key_arn     = module.s3_external_kms_key.kms_key_arn
   common_tags     = local.common_tags
-  lifecycle_rules = local.export_bucket_lifecycle_rules
+  lifecycle_rules = local.environment == "prod" ? [] : local.non_prod_default_bucket_lifecycle_rules
 }
 
 module "external_sns_notifications_topic" {
@@ -552,7 +552,7 @@ module "export_bucket" {
   read_access_role_arns     = local.standard_export_bucket_read_access_roles
   bucket_policy             = "export_bucket"
   s3_bucket_additional_tags = local.aws_back_up_tags
-  lifecycle_rules           = local.export_bucket_lifecycle_rules
+  lifecycle_rules           = local.environment == "prod" ? [] : local.non_prod_default_bucket_lifecycle_rules
 }
 
 module "export_bucket_judgment" {
@@ -564,7 +564,7 @@ module "export_bucket_judgment" {
   bucket_key_enabled    = true
   read_access_role_arns = local.judgment_export_bucket_read_access_roles
   bucket_policy         = "export_bucket"
-  lifecycle_rules       = local.export_bucket_lifecycle_rules
+  lifecycle_rules       = local.environment == "prod" ? [] : local.non_prod_default_bucket_lifecycle_rules
 }
 
 module "notifications_topic" {
--- root_s3_object_lifecycle.tf
+++ root_s3_object_lifecycle.tf
@@ -1,11 +1,8 @@
 locals {
-  default_expiration_days = local.environment == "prod" ? 30 : 7
+  default_expiration_days          = local.environment == "prod" ? 30 : 7
+  default_non_prod_expiration_days = 30
 
   backend_checks_bucket_policy_status = "Enabled"
-  clean_bucket_policy_status          = local.environment == "prod" ? "Disabled" : "Enabled"
-  dirty_bucket_policy_status          = local.environment == "prod" ? "Disabled" : "Enabled"
-  export_bucket_policy_status         = local.environment == "prod" ? "Disabled" : "Enabled"
-  quarantine_bucket_policy_status     = local.environment == "prod" ? "Disabled" : "Enabled"
 
   backend_checks_results_bucket_lifecycle_rules = [{
     id     = "delete-backend-checks-results-bucket-objects"
@@ -18,46 +15,22 @@ locals {
     }
   }]
 
-  clean_bucket_lifecycle_rules = [
+  non_prod_default_bucket_lifecycle_rules = [
     {
-      id     = "delete-clean-bucket-objects"
-      status = local.clean_bucket_policy_status
+      id     = "delete-bucket-objects"
+      status = "Enabled"
       expiration = {
-        days = local.default_expiration_days
+        days = local.default_non_prod_expiration_days
       }
       noncurrent_version_expiration = {
-        noncurrent_days = local.default_expiration_days
+        noncurrent_days = local.default_non_prod_expiration_days
       }
   }]
 
   dirty_bucket_lifecycle_rules = [
     {
       id     = "delete-dirty-bucket-objects"
-      status = local.dirty_bucket_policy_status
-      expiration = {
-        days = local.default_expiration_days
-      }
-      noncurrent_version_expiration = {
-        noncurrent_days = local.default_expiration_days
-      }
-  }]
-
-  export_bucket_lifecycle_rules = [
-    {
-      id     = "delete-export-bucket-objects"
-      status = local.export_bucket_policy_status
-      expiration = {
-        days = local.default_expiration_days
-      }
-      noncurrent_version_expiration = {
-        noncurrent_days = local.default_expiration_days
-      }
-  }]
-
-  quarantine_bucket_lifecycle_rules = [
-    {
-      id     = "delete-quarantine-bucket-objects"
-      status = local.quarantine_bucket_policy_status
+      status = "Enabled"
       expiration = {
         days = local.default_expiration_days
       }
