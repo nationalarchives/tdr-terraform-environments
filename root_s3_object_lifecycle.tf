9e7e58bc8c62a8320bf00052afc04d88f9558838
TomJKing <tom.king@nationalarchives.gov.uk>
2025-03-20 11:05:39 +0000

    Fix prod dirty bucket rule

--- root_s3_object_lifecycle.tf
+++ root_s3_object_lifecycle.tf
@@ -3,6 +3,7 @@ locals {
   default_non_prod_expiration_days = 30
 
   backend_checks_bucket_policy_status = "Enabled"
+  dirty_bucket_policy_status          = local.environment == "prod" ? "Disabled" : "Enabled"
 
   backend_checks_results_bucket_lifecycle_rules = [{
     id     = "delete-backend-checks-results-bucket-objects"
@@ -30,7 +31,7 @@ locals {
   dirty_bucket_lifecycle_rules = [
     {
       id     = "delete-dirty-bucket-objects"
-      status = "Enabled"
+      status = local.dirty_bucket_policy_status
       expiration = {
         days = local.default_expiration_days
       }
