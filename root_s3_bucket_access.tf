ddfa75ba603363475441d37476c1151d875ef1e9
TomJKing <tom.king@nationalarchives.gov.uk>
2025-05-19 09:06:50 +0100

    Example

--- root_s3_bucket_access.tf
+++ root_s3_bucket_access.tf
@@ -2,7 +2,7 @@
 
 locals {
   aws_sso_export_bucket_access_roles   = local.environment == "prod" ? [] : [data.aws_ssm_parameter.aws_sso_admin_role.value, data.aws_ssm_parameter.aws_sso_export_role.value]
-  aws_sso_internal_bucket_access_roles = local.environment == "prod" ? [] : [data.aws_ssm_parameter.aws_sso_admin_role.value]
+  aws_sso_internal_bucket_access_roles = local.environment == "prod" ? [data.aws_ssm_parameter.aws_sso_admin_role.value] : [data.aws_ssm_parameter.aws_sso_admin_role.value]
 }
 
 data "aws_ssm_parameter" "aws_sso_admin_role" {
