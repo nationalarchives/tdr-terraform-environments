79949f01759cc881d40163811d18968af6eb8db0
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2025-06-19 18:29:54 +0100

    chore: Belt and braces: dynamically generate arn partition

--- modules/shared-vpc/root.tf
+++ modules/shared-vpc/root.tf
@@ -3,6 +3,7 @@ data "aws_availability_zones" "available" {
 
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+data "aws_partition" "current" {}
 
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
