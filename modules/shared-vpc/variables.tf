4f618d285112ce8ecd3ebd5d134f34b61683728f
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2025-06-20 09:01:22 +0100

    chore: Make param name more descriptive

--- modules/shared-vpc/variables.tf
+++ modules/shared-vpc/variables.tf
@@ -2,8 +2,8 @@ variable "environment" {}
 variable "common_tags" {}
 variable "az_count" {}
 variable "database_availability_zones" {}
-variable "cloudwatch_retention_period" {
-  description = "Retention period for the cloudwatch log group where flow logs are kept"
+variable "cloudwatch_retention_period_days" {
+  description = "Retention period in days for the cloudwatch log group where flow logs are kept"
   default     = 30
 }
 
