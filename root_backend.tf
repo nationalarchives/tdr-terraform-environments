f2e956f39ab6624fc2fc8f5b65d3828ee6915a4f
Ben S <122529331+ben-cerium@users.noreply.github.com>
2025-07-24 10:46:35 +0100

    Tdrd 825/upgrade tf 12 (#658)
    * chore: Upate to TF 1.12.2, submodule bump (needed for provider v6) and bump provider to v6
    
    * chore: Add S3 lockfile, update README, update GitHub provider

--- root_backend.tf
+++ root_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tdr-terraform-state"
-    key            = "terraform.state"
-    region         = "eu-west-2"
-    encrypt        = true
-    dynamodb_table = "tdr-terraform-state-lock"
+    bucket       = "tdr-terraform-state"
+    key          = "terraform.state"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.76.0"
+      version = "~> 6.4"
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.12"
+      version = "~> 6.6"
     }
   }
-  required_version = ">= 1.9.8"
+  required_version = ">= 1.12.2"
 }
