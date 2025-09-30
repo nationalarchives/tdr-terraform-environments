1c961de1c26ed6cd93194ef66658fe772884e4a8
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-09-30 10:44:39 +0100

    730 day inactivity

--- root.tf
+++ root.tf
@@ -776,7 +776,7 @@ module "disable_inactive_judgment_users_scheduled_event" {
   lambda_event_target_arn = module.inactive_keycloak_users_lambda.lambda_arn
   input = jsonencode({
     userType             = "judgment_user"
-    inactivityPeriodDays = 770
+    inactivityPeriodDays = 730
   })
 }
 
