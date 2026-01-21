eb7c9d052ef3025319b97dad9d4e8b69a6307f33
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2026-01-21 14:17:21 +0000

    fmt

--- root_athena_analytics.tf
+++ root_athena_analytics.tf
@@ -13,8 +13,8 @@ module "athena_reporting_results_s3" {
 }
 
 module "athena_reporting_analytics" {
-  source = "./da-terraform-modules/athena"
-  name   = "tdr_reporting_analytics"
+  source             = "./da-terraform-modules/athena"
+  name               = "tdr_reporting_analytics"
   result_bucket_name = local.athena_results_bucket_name
   create_table_queries = {
     metadata_validation_reports = templatefile("${path.module}/templates/athena/metadata_validation_reports.sql.tpl", {
@@ -22,5 +22,5 @@ module "athena_reporting_analytics" {
     })
   }
   common_tags = local.common_tags
-  kms_key_arn =  module.s3_internal_kms_key.kms_key_arn
+  kms_key_arn = module.s3_internal_kms_key.kms_key_arn
 }
