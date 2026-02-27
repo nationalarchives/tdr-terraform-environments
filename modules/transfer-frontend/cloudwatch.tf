a02e58a2b21ce1aac95e90c0b37077ef2a46a115
Ben S <122529331+ben-cerium@users.noreply.github.com>
2026-02-27 12:21:13 +0000

    fix: Broken params (#809)

--- modules/transfer-frontend/cloudwatch.tf
+++ modules/transfer-frontend/cloudwatch.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_log_group" "frontend_log_group" {
 
 resource "aws_cloudwatch_log_stream" "tdr_application_log_stream" {
   name           = "tdr-frontend-log-stream-${var.environment}"
-  log_group_name = var.cloudwatch_log_retention_in_days
+  log_group_name = aws_cloudwatch_log_group.frontend_log_group.name
 }
 
 resource "aws_cloudwatch_log_group" "aws-otel-collector" {
