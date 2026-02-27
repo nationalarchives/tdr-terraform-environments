d49e02b4cf699d791dee3b514173f6b88dd09677
Ben S <122529331+ben-cerium@users.noreply.github.com>
2026-02-27 09:43:33 +0000

    chore: Use params for logging retention (#804)
    * chore: Use params for logging retention

--- modules/transfer-frontend/variables.tf
+++ modules/transfer-frontend/variables.tf
@@ -87,3 +87,8 @@ variable "metadata_version_override" {
   type        = string
 }
 
+variable "cloudwatch_log_retention_in_days" {
+  description = "Cloudwatch log retention period in days (0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653)"
+  default     = 30
+}
+
