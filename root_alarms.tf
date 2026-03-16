fbf94ef02fb88e447e7c3fdaf96e676831718f1c
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-03-16 14:56:27 +0000

    chore: Alarm for tdr-upload-files-quarantine in all envs

--- root_alarms.tf
+++ root_alarms.tf
@@ -1,16 +1,15 @@
 # TDRD-1268
 # All the infrastructure alarms go here
-# Alarms must be created in the management account
+# Alarms must be created in the management account 
 # Naming convention for the alarm should be
 # <metrics_name space>-<why>-<resource_triggering_alert>
 
-# This assume that the thing contains an account identifier.  If not add one in the alarn name
+# This assume that the resource contains an account identifier.  If not add one in the alarm name
 #
 # S3
-resource "aws_cloudwatch_metric_alarm" "tdr_alarms_s3_object_put" {
-  count             = local.environment == "intg" ? 1 : 0
+resource "aws_cloudwatch_metric_alarm" "tdr_alarms_s3_object_put_quarantine" {
   alarm_description = "This alarm fires when a object has been put into the bucket"
-  alarm_name        = format("AWS/S3 Object Put in tdr-upload-files-%s", local.environment)
+  alarm_name        = format("AWS/S3 Object Put in tdr-upload-files-quarantine-%s", local.environment)
 
   metric_query {
     account_id  = data.aws_caller_identity.current.id
@@ -23,7 +22,7 @@ resource "aws_cloudwatch_metric_alarm" "tdr_alarms_s3_object_put" {
       stat        = "Sum"
       period      = 60
       dimensions = {
-        BucketName = format("tdr-upload-files-%s", local.environment)
+        BucketName = format("tdr-upload-files-quarantine-%s", local.environment)
         FilterId   = "EntireBucket"
       }
     }
