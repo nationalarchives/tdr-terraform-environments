66582720e00c6f8630fcc6c6c73005d1926007a2
ian-hoyle <ian.hoyle.tna@gmail.com>
2026-03-17 10:28:31 +0000

    provide statuses lambda permissions (#827)

--- root_backend_checks.tf
+++ root_backend_checks.tf
@@ -230,9 +230,12 @@ module "statuses" {
   cloudwatch_log_retention_in_days = module.global_parameters.policy_cloudwatch_logs_retention["${local.environment}"].lambda
   policies = {
     "TDRStatusesLambdaPolicy${title(local.environment)}" = templatefile("./templates/iam_policy/lambda_statuses_policy.json.tpl", {
-      function_name = local.statuses_function_name,
-      account_id    = data.aws_caller_identity.current.account_id,
-      bucket_name   = module.backend_lambda_function_bucket.s3_bucket_name
+      function_name      = local.statuses_function_name,
+      account_id         = data.aws_caller_identity.current.account_id,
+      bucket_name        = module.backend_lambda_function_bucket.s3_bucket_name,
+      client_secret_path = local.keycloak_backend_checks_secret_name,
+      sns_topic_arn      = module.notifications_topic.sns_arn,
+      kms_key_arn        = module.encryption_key.kms_key_arn
     })
   }
   role_name = "TDRStatusesLambdaRole${title(local.environment)}"
