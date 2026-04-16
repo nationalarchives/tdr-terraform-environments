b34d490b6b16e6cb0c5e1d77f55fe002625dd51e
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-04-16 18:24:45 +0100

    chore: WIP add SG

--- modules/shared-vpc/root.tf
+++ modules/shared-vpc/root.tf
@@ -145,3 +145,25 @@ resource "aws_vpc_endpoint_route_table_association" "s3_endpoint_private" {
   route_table_id  = aws_route_table.private.*.id[count.index]
   vpc_endpoint_id = aws_vpc_endpoint.s3_endpoint.id
 }
+
+# TDRD-964 ECR Endpoint
+resource "aws_security_group" "endpoint_security_group" {
+  name        = "tdr-endpoint-security-group"
+  description = "Allow 443 inbound from VPC only"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    tomap(
+      { "Name" = "tdr-endpoint-security-group" }
+    )
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "endpoint_security_group_tls_ingress" {
+  security_group_id = aws_security_group.endpoint_security_group.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
