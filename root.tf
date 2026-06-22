729c4773965341f34073a21d4425950066be32cd
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-06-22 18:20:39 +0100

    chore: use list of public certs

--- root.tf
+++ root.tf
@@ -238,7 +238,7 @@ module "cloudfront_upload" {
   certificate_arn                     = module.cloudfront_certificate.certificate_arn
   api_gateway_url                     = module.signed_cookies_api.api_url
   waf_arn                             = local.environment == "intg" || local.environment == "dev" ? module.cloudfront_waf_non_prod[0].aws_wafv2_web_acl.arn : module.cloudfront_waf_prod[0].aws_wafv2_web_acl.arn
-  signed_cookie_public_key_names      = ["sign_cookies_public_key_${local.environment}.pem"]
+  signed_cookie_public_key_names      = module.global_parameters.signed_cookie_key["${local.environment}"].public_key_file_name
 }
 
 module "cloudfront_upload_dns" {
