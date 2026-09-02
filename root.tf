10aa131171c67a188787c53430d923925e38fc34
Ben S <122529331+ben-cerium@users.noreply.github.com>
2026-09-02 15:10:15 +0100

    Tdrd 1710/create keycloak users api disable for prod (#968)
    * chore: Add a count to CreateKeycloakUsersApi
    
    * fix: Allow for lambda to be created without API Gateway
    
    * chore: Submodule bump for api gateway

--- root.tf
+++ root.tf
@@ -783,7 +783,7 @@ module "create_keycloak_users_api_lambda" {
   vpc_id                           = module.shared_vpc.vpc_id
   lambda_create_keycloak_user_api  = true
   private_subnet_ids               = module.shared_vpc.private_backend_checks_subnets
-  keycloak_user_management_api_arn = module.create_keycloak_users_api.api_arn
+  keycloak_user_management_api_arn = one(module.create_keycloak_users_api[*].api_arn)
   cloudwatch_log_retention_in_days = module.global_parameters.policy_cloudwatch_logs_retention["${local.environment}"].lambda
 }
 
@@ -854,6 +854,7 @@ module "disable_inactive_judgment_users_scheduled_event" {
 }
 
 module "create_keycloak_users_api" {
+  count                            = local.environment != "prod" ? 1 : 0
   source                           = "./tdr-terraform-modules/apigatewayv2"
   body_template                    = templatefile("${path.module}/templates/api_gateway/create_keycloak_users.json.tpl", { region = local.region, lambda_arn = module.create_keycloak_users_api_lambda.create_keycloak_users_api_lambda_arn, auth_url = local.keycloak_auth_url })
   environment                      = local.environment
@@ -861,6 +862,13 @@ module "create_keycloak_users_api" {
   common_tags                      = local.common_tags
   cloudwatch_log_retention_in_days = module.global_parameters.policy_cloudwatch_logs_retention["${local.environment}"].api_gateway
 }
+
+# TDRD-1710
+moved {
+  from = module.create_keycloak_users_api
+  to   = module.create_keycloak_users_api[0]
+}
+
 
 module "create_bulk_users_bucket" {
   source              = "./tdr-terraform-modules/s3"
